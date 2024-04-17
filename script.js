483890a2cd33b4af86cc969a500eee4ea156ef46
Otaku1120 <157336349+Otaku1120@users.noreply.github.com>
2024-04-17 04:51:04 +0000

    3 vs 1 move when pressing keyboard

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 /**
  * Project 3 versions 0-4 - 2D Web Game
  * Name:
- * 
+ * Anahi Paz- vs 1
  * Use this template to get started creating a simple 2D game for the web using P5.js. 
  */
 var gameState = "splash";
+var player1;
+
 function setup() {
 
   createCanvas(600, 400);
+  player1= new Player(width/2, height* 7/8);
+  console.log(player1);
 
 }
 
 function draw() {
   background(200);
   switch (gameState) {
-    case "splash" :
-  splash(); // call the splash screen function (below)
-  break;
-  
-  case "play" ://play(); // call the play screen function (below)
+    case "splash":
+      splash(); // call the splash screen function (below)
+      break;
+  case "play"://play(); // call the play screen function (below)
   play();
   break;
-  
-  //gameOver(); // call the gameOver screen function (below)
-  case "gameOver" :
+   // call the gameOver screen function (below)
+  case "gameOver":
     gameOver(); // go to the "game over" screen
     break;
     default :
@@ -48,7 +50,10 @@ function play() {
   textAlign(CENTER);
   textSize(16);
   text("This is where the Game happens", width / 2, height / 2);
-
+  //mouse controls
+  //player1.x = mouseX;
+  //player1.y = mouseY;
+  player1.display();
 }
 
 function gameOver() {
@@ -68,5 +73,32 @@ function mousePressed() {
   } else if (gameState === "gameOver") {
     gameState = "splash";
   }
-  console.log("click!");
+  console.log(gameState);
+}
+
+function keyPressed(){
+  switch(keyCode){
+    case UP_ARROW :
+      player1.y -= 30;// sub 38 from .y
+      player1.angle = 0;
+      if(player1.y<0) player1.y=height;
+      break;
+    case DOWN_ARROW:
+      player1.y += 30;
+      player1.angle = PI;
+      if(player1.y>height) player1.y=0;
+      break;
+    case LEFT_ARROW:
+      player1.x -= 30;
+      player1.angle= PI + PI/2;
+      if(player1.x<0) player1.x = width;
+      break;
+    case RIGHT_ARROW:
+      player1.x +=30;
+      player1.angle= PI/2;
+      if(player1.x>width) player1.x=0;
+      break;
+    default:
+      console.log("use the arrow keys to move!");
+  }
 }
